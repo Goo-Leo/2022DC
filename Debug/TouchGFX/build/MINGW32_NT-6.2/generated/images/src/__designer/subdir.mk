be57b6f83b497e57b2d153ccaa9f5093eafa4187
Luo-freewell-man <https://github.com/Luo-freewell-man>
2022-03-11 16:31:52 +0800

    deployed the rtos and gui

---
+++ Debug/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer/subdir.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer/image_Blue_Buttons_Round_Edge_small.o \
+../TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer/image_Blue_Buttons_Round_Edge_small_pressed.o \
+../TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer/image_Blue_PageIndicator_dot_indicator_small_highlight.o \
+../TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer/image_Blue_PageIndicator_dot_indicator_small_normal.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
